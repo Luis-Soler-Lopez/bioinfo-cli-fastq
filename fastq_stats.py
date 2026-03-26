5f527344b6330b795a893dd5befa91ca7b95300f
Luis <luis.sol.lop@gmail.com>
2026-03-26 14:25:16 +0100

    Separate logic and CLI

--- fastq_stats.py
+++ fastq_stats.py
@@ -1,40 +1,6 @@
 import argparse
 
-def count_reads(file_path):
-    contador = 0
-
-    with open(file_path, "r") as f:
-        for linea in f:
-            contador += 1
-
-    reads = contador // 4
-    return reads
-
-def average_read_length(file_path):
-	total_length = 0
-	num_reads = 0
-
-	with open(file_path, "r") as f:
-		for i, linea in enumerate(f):
-			if i % 4 == 1:
-				seq = linea.strip()
-				total_length += len(seq)
-				num_reads += 1
-
-	return total_length / num_reads
-
-def gc_content(file_path):
-	gc = 0
-	total = 0
-
-	with open(file_path,"r") as f:
-		for i, linea in enumerate(f):
-			if i % 4 == 1:
-				seq = linea.strip().upper()
-				gc += seq.count("G") + seq.count("C")
-				total += len(seq)
-
-	return gc / total
+from core import count_reads, average_read_length, gc_content
 
 def main():
 	parser = argparse.ArgumentParser(description="FASTQ basic statistics")
